b36786c182ca9e7681140d4f1d093a2acdd99f7f
Victor-Muriithi <vmuriithi10@gmail.com>
2024-05-29 00:08:29 +0300

    custom error page

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,23 +1,9 @@
 "use client";
-
-import { useRouter } from "next/router";
 export default function ErrorBoundary({ error }: { error: Error }) {
-
-
-    const router = useRouter();
-  const handleGoBack = () => {
-    console.log("backroute");
-    if (window.history.length > 1) {
-      router.back();
-    } else {
-      router.push('/'); // Redirect to home or any other default page
-    }
-  };
   return (
     <>
       {" "}
       <h1>{error.message}</h1>
-      <button onClick={handleGoBack}>go back</button>
     </>
   );
 }
